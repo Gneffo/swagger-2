07fa054d91b6cd2ed907adf31b1ef8b07ae231fd
Michele Junior Cioffo <michelecioffo_98@hotmail.com>
2023-02-22 10:16:05 +0100

    Commit

--- src/main/java/co/develhope/swagger2/configurations/SpringDocConfig.java
+++ src/main/java/co/develhope/swagger2/configurations/SpringDocConfig.java
@@ -6,14 +6,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.Collections;
+
 @OpenAPIDefinition(info =
 @Info(
         title = "swagger",
